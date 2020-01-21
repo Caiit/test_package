5bcdfd6c8df2f9a2182f59214adfa7a87c70f4f3
Caiit <Caiit@users.noreply.github.com>
2020-01-21 13:13:21 +0100

    Add test package

---
+++ package/README.md
@@ -0,0 +1,2 @@
+https://stackoverflow.com/questions/6323860/sibling-package-imports/50193944#50193944
+https://www.freecodecamp.org/news/how-to-use-github-as-a-pypi-server-1c3b0d07db2/
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='package',
+    version='0.0.1',
+    description='a pip-installable package example',
+    packages=find_packages(),
+    keywords=['example']
+)
